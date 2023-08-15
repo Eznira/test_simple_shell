cbe4b2534cd8c62ccdcf9a07b05ba5409160e416
Eznira <ihimarinze@gmail.com>
2023-08-15 15:23:34 +0100

    local test 2

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "shell.h"
 
 int main(void)
@@ -29,4 +30,62 @@ int main(void)
 	free(command);
 
 	return 0;
+=======
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define MAX_INPUT_LENGTH 1024
+
+int main() {
+    char input[MAX_INPUT_LENGTH];
+
+    while (1) {
+        printf("MyShell> ");
+        fflush(stdout);
+
+        if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) {
+            break;
+        }
+
+        // Remove the trailing newline character
+        if (input[strlen(input) - 1] == '\n') {
+            input[strlen(input) - 1] = '\0';
+        }
+
+        // Parse the input using strtok
+        char *token = strtok(input, " ");
+        char *args[MAX_INPUT_LENGTH]; // Array to hold command and arguments
+        int i = 0;
+
+        while (token != NULL) {
+            args[i] = token;
+            i++;
+            token = strtok(NULL, " ");
+        }
+
+        args[i] = NULL; // Set the last element to NULL for execvp
+
+        pid_t child_pid = fork(); // Create a child process
+
+        if (child_pid < 0) {
+            perror("Fork failed");
+        } else if (child_pid == 0) {
+            // Child process: execute the command
+            execvp(args[0], args);
+            perror("Execvp failed");
+            exit(1);
+        } else if (input == "exit") {
+              break;
+        } else  {
+            // Parent process: wait for the child to complete
+            waitpid(child_pid, NULL, 0);
+        } 
+    }
+
+    return 0;
+>>>>>>> d99a9da (local test)
 }

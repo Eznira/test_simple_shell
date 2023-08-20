83871098b11373bd741b3b6c61d4a74a85844d26
Eznira <ihimarinze@gmail.com>
2023-08-20 16:32:20 -0700

    ,added _printf func

--- util_lib.c
+++ util_lib.c
@@ -56,3 +56,28 @@ ssize_t _getline(char **line_input_ptr, size_t *n, FILE *stream) {
     *n = buff_size;
     return (ssize_t)index;
 }
+
+/**
+ * _strcmp - Compare two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return:
+ *   0 if the strings are equal
+ *   a negative value if s1 is less than s2
+ *   a positive value if s1 is greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+
+	return (s1[i] - s2[i]);
+}

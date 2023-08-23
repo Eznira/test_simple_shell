05c22e41743d9f4f7bb719d5a58a37cf746a13fa
Eznira <ihimarinze@gmail.com>
2023-08-23 06:15:01 -0700

    betty compliant

--- betty_check/main.c
+++ betty_check/main.c
@@ -7,16 +7,12 @@
  */
 int main(void)
 {
-	char *input = NULL;
+	int i;
 	size_t input_size = 0;
-
-	char *command = NULL;
-	char *args[MAX_INPUT_SIZE];
-	int is_background = 0, i;
+	char *input = NULL, *args[MAX_INPUT_SIZE], *command = NULL;
 
 	while (1)
 	{
-		/* Display prompt */
 		printf("$ ");
 		get_input(&input, &input_size);
 
@@ -25,14 +21,6 @@ int main(void)
 		{
 			printf("\n");
 			break;
-		}
-
-		/* Check for background execution */
-		is_background = 0;
-		if (input[strlen(input) - 1] == '&')
-		{
-			is_background = 1;
-			input[strlen(input) - 1] = '\0';
 		}
 
 		/* Tokenize input into command and arguments */
@@ -50,7 +38,6 @@ int main(void)
 
 		if (i == num_builtins)
 		{
-			/* Create a child process */
 			pid_t child_pid = fork();
 
 			if (child_pid < 0)
@@ -59,18 +46,13 @@ int main(void)
 			}
 			else if (child_pid == 0)
 			{
-				/* Child process: execute the command */
 				execvp(args[0], args);
 				perror("Execvp failed");
 				exit(1);
 			}
 			else
 			{
-				if (!is_background)
-				{
-					/* Parent process: wait for the child to complete */
-					waitpid(child_pid, NULL, 0);
-				}
+				waitpid(child_pid, NULL, 0);
 			}
 		}
 	}
